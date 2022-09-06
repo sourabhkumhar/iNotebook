7b93bc824a8eeafa16e5456ea95f506bdbd70215
sourabhkumhar <mr.sourabhkumhar@gmail.com>
2022-09-06 20:41:36 +0530

    Update NoteState.js

--- frontend/src/context/notes/NoteState.js
+++ frontend/src/context/notes/NoteState.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-    const host = "http://localhost:5000";
+    const host = "https://i-notebook-flame.vercel.app/";
     const notesInitial = []
     const [notes, setNotes] = useState(notesInitial)
 
